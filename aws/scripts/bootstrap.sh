3912641fed0cd375a52482a6f0426111baaf68f5
OtariOboladze <otari.oboladze@outlook.com>
2024-11-10 23:27:15 +0100

    let the code snippet run wwithout conflicting prior commands temporarly

--- aws/scripts/bootstrap.sh
+++ aws/scripts/bootstrap.sh
@@ -13,13 +13,13 @@ CLUSTER_NAME=${3:-cluster}
 # quourum
 AKS_NAMESPACE=${4:-quorum}
 
-echo "aws get-credentials ..."
-aws sts get-caller-identity
-aws eks --region "${AWS_REGION}" update-kubeconfig --name "${CLUSTER_NAME}"
+# echo "aws get-credentials ..."
+# aws sts get-caller-identity
+# aws eks --region "${AWS_REGION}" update-kubeconfig --name "${CLUSTER_NAME}"
 
-helm repo add secrets-store-csi-driver https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts
-helm install --namespace kube-system --create-namespace csi-secrets-store secrets-store-csi-driver/secrets-store-csi-driver
-kubectl apply -f https://raw.githubusercontent.com/aws/secrets-store-csi-driver-provider-aws/main/deployment/aws-provider-installer.yaml 
+# helm repo add secrets-store-csi-driver https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts
+# helm install --namespace kube-system --create-namespace csi-secrets-store secrets-store-csi-driver/secrets-store-csi-driver
+# kubectl apply -f https://raw.githubusercontent.com/aws/secrets-store-csi-driver-provider-aws/main/deployment/aws-provider-installer.yaml 
 
 # If you have deployed the above policy before, acquire its ARN:
 POLICY_ARN=$(aws iam list-policies --scope Local --query 'Policies[?PolicyName==`quorum-node-secrets-mgr-policy`].Arn' --output text)
